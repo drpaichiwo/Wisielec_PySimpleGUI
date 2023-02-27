e6927b3f2dbbdf31976f8cb5722662a8f56cd89a
Lukasz Zerucha <37277099+drpaichiwo@users.noreply.github.com>
2023-02-27 16:19:16 +0000

    Add files via upload

---
+++ Wisielec_PySimpleGUI.py
@@ -0,0 +1,116 @@
+# Wisielec with PySimpleGUI
+
+import PySimpleGUI as sg
+import random
+import time
+from word_dictionary import words
+from hangman_art import hangman_art
+
+sg.theme("black")
+font_used = "Young"
+layout = [
+    [sg.Image(hangman_art[10], key="-HANGMAN-")],
+    [sg.Text("", key="-WORD-", font="Young 20")],
+    [sg.Text("użyte litery", font=font_used)],
+    [sg.Text("", key="-USED-LETTERS-", font=font_used)],
+    [sg.Text("życia", font=font_used),
+     sg.Text("", key="-LIVES-", font="Young 16", text_color="green")],
+    [sg.Text("Podaj literę:", font=font_used)],
+    [sg.Input("", size=(10, 1),
+              enable_events=True,
+              key="-INPUT-")],
+    [sg.Button('Submit', visible=False, bind_return_key=True)],
+    [sg.Text("", key="-OUT-", font="Any 10", text_color="yellow")],
+    [sg.VPush()]
+]
+
+window = sg.Window("Wisielec", layout,
+                   size=(300, 550),
+                   element_justification="center",
+                   finalize=True)
+
+
+def hangman():
+
+    # list with polish alphabet
+    alphabet = ['A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł',
+                'M', 'N', 'Ń', 'O', 'Ó', 'P', 'R', 'S', 'Ś', 'T', 'U', 'W', 'Y', 'Z', 'Ź', 'Ż',
+                'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł',
+                'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż']
+
+    # word choice
+    word = random.choice(words).upper()
+    # word letters as list
+    word_letters = [i for i in word]
+    # word blanks as list
+    word_blanks = ["_"] * len(word)
+    # number of tries
+    lives = 10
+    # letters already used
+    guessed_letters = ""
+
+    # Main Loop
+    while lives > 0:
+
+        window["-HANGMAN-"].update(hangman_art[lives])
+        window["-WORD-"].update("".join(word_blanks), font="Young 24")
+        window["-USED-LETTERS-"].update(", ".join(guessed_letters))
+        window["-LIVES-"].update(lives)
+
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+
+        # accepts only one character, must be from alphabet
+        if len(values["-INPUT-"]) > 1 or values["-INPUT-"] not in alphabet:
+            # delete last char from input
+            window["-INPUT-"].update(values["-INPUT-"][:-1])
+        # if enter is pressed
+        elif event == 'Submit':
+            user_input = window["-INPUT-"].get().upper()
+            window['-INPUT-'].update("")
+
+            # Check if user_input in guessed letters
+            if user_input not in guessed_letters:
+                guessed_letters += user_input
+                window["-USED-LETTERS-"].update(", ".join(guessed_letters))
+
+                # Check if user_input is in word_letters list
+                if user_input in word_letters:
+                    window["-OUT-"].update("Zgadłeś literę. Brawo")
+
+                    # find indexes of correctly guessed letter
+                    for i, letter in enumerate(word_letters):
+                        # and replace blanks with letters
+                        if word_letters[i] == user_input:
+                            word_blanks[i] = word[i]
+                    # output updated word
+                    window["-WORD-"].update("".join(word_blanks), font="Young 24")
+
+                    # win condition
+                    if "".join(word_blanks) == "".join(word_letters):
+                        window["-OUT-"].update("Wygrałeś! Brawo!")
+                        time.sleep(2)
+                        sg.popup("Wygrana, losuję nowe słowo", font=font_used)
+                        hangman()
+                # lost condition
+                else:
+                    lives = lives - 1
+                    window["-OUT-"].update("Pudło, spróbuj jeszcze raz")
+                    window["-HANGMAN-"].update(hangman_art[lives])
+                    window["-LIVES-"].update(lives)
+                    window["-WORD-"].update("".join(word_blanks), font="Young 24")
+                    if lives == 0:
+                        window["-OUT-"].update("Przegrałeś!")
+                        time.sleep(2)
+                        sg.popup("Przegrana. Losuję nowe słowo", font=font_used)
+                        hangman()
+            # output if letter already been chosen
+            else:
+                window["-OUT-"].update("Ta litera już była.")
+
+    window.close()
+
+
+if __name__ == '__main__':
+    hangman()
---
+++ hangman_art.py
@@ -0,0 +1,14 @@
+hangman_art = {
+
+    10: "images/Wisielec_10.png",
+    9: "images/Wisielec_9.png",
+    8: "images/Wisielec_8.png",
+    7: "images/Wisielec_7.png",
+    6: "images/Wisielec_6.png",
+    5: "images/Wisielec_5.png",
+    4: "images/Wisielec_4.png",
+    3: "images/Wisielec_3.png",
+    2: "images/Wisielec_2.png",
+    1: "images/Wisielec_1.png",
+    0: "images/Wisielec_0.png",
+}
